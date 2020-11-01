360616e2beeeb815d6458afc885f9012c94e1ee3
hleerubin <35813588+hleerubin@users.noreply.github.com>
2020-11-01 17:09:06 -0500

    Update spotipy_functions.py

--- spotipy_functions.py
+++ spotipy_functions.py
@@ -2,13 +2,15 @@ import spotipy
 import sys
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
-from passwords import *  
+# from passwords import *  
 
 
 redirectURL = 'http://localhost/' # We/Spotify suggest http://localhost:8888/callback/ or http://localhost/
 scope = 'playlist-modify-public'
 username = 'aminvikram'
 
+clientID = 'f30f57ef46074ab2aed92607b5b0301c'
+clientSecret = 'ecf7fd579360466ba6cf1513137d0cfe'
 
 
 client_credentials_manager = SpotifyClientCredentials(client_id=clientID,
